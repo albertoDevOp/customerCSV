e928b7e4f4f71c1c40d765716080f5ae09bd12cf
alberto rodrigues <jrdawn@gmail.com>
2019-08-24 13:37:13 +0200

    general fixes

--- crm/main.go
+++ crm/main.go
@@ -1,20 +1,17 @@
 package main
 
-import {
-	"fmt",
+import (
+	"fmt"
 	"net/http"
-}
+)
 
 func main() {
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Welcome to my website!")
+    })
 
-	http.HandleFunc('/', func (w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w, "Welcome to my website!")
-	})
-
-	fs := http.FileServer(http.Dir("static/"))
+    fs := http.FileServer(http.Dir("static/"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	if err := http.ListenAndServe(":5000", nil); err != nil {
-		panic(err)
-	}
+    http.ListenAndServe(":5000", nil)
 }
---
+++ up.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+export PG_USER=root
+export PG_PWD=root
+docker-compose up -d -f ./docker-compose.yml
